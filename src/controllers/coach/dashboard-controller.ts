988fc481d6af2d1f1418a1d4b63812d850449374
Ahmed Mostafa <ahmed2303479@miuegypt.edu.eg>
2026-09-15 16:18:33 +0300

    feat(coach): import Types from mongoose for enhanced data handling in dashboard controller

--- src/controllers/coach/dashboard-controller.ts
+++ src/controllers/coach/dashboard-controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { Types } from "mongoose";
 import asyncHandler from "../../utils/asyncHandler";
 import { SuccessResponse } from "../../core/ApiResponse";
 import { CoachService } from "../../services/coach-service";
